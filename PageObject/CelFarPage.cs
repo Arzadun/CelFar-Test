60b65fc2ab2924621182ff26a89490b6468aeb54
Arzadun <55960691+Arzadun@users.noreply.github.com>
2020-02-24 22:01:39 -0300

    Agregar archivos de proyecto.

---
+++ PageObject/CelFarPage.cs
@@ -0,0 +1,74 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CelFar_Test.PageObject
+{
+    public class CelFarPage
+    {
+        protected IWebDriver Driver;
+
+        static void Main(string[] args)
+
+        { 
+
+        }
+
+        //Locators
+        protected By InputField = By.Id("input");
+        protected By InputButton = By.ClassName("button");
+        
+        
+        //Constructor. If the title of the page is not right throws an exception
+        public CelFarPage (IWebDriver driver)
+        {
+            Driver = driver;
+            if (!Driver.Title.Equals("Conversor CelFar"))
+                throw new Exception("this is not the CelFar page");
+        }
+            
+        //set the information on the locators
+        public void FillInput(string input)
+        {
+            Driver.FindElement(InputField).SendKeys(input);
+        }
+
+        public void ClickSubmit()
+        {
+            Driver.FindElement(InputButton).Click();
+        }
+
+        //Method for the assert
+        public string ConversorCelFar(int celsius)
+        {
+            int expectedResult = (celsius * 9 / 5) + 32;
+            string FinalResult = expectedResult.ToString();
+            return FinalResult;
+        }
+        
+        //Submit the number for the assert 
+        //Wait for two seconds until the page changes the output field
+        public string SubmitInfo(string input)
+        {
+            FillInput(input);
+            ClickSubmit();
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(2));
+            return input;
+        }
+
+
+
+
+
+
+        
+         
+        
+
+    }
+}
---
+++ TestCases/TestCases.cs
@@ -0,0 +1,53 @@
+﻿using CelFar_Test.PageObject;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelFar_Test.TestCases
+{   
+    [TestFixture]
+    public class TestCases
+    {
+        protected IWebDriver Driver;
+
+        [SetUp]
+        //Go to the URL and maximize the page
+        public void BeforeTest()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("https://nahual.github.io/qc-celfar/");
+            
+
+        }
+
+        [Test]
+
+        public void ActualTest()
+        {
+            //Set the info to submit and compare
+            CelFarPage celFarPage = new CelFarPage(Driver);
+            var Result = celFarPage.SubmitInfo("30");
+            IWebElement ResultOutput = Driver.FindElement(By.Id("output"));
+            //Assert 
+            String ActualResult = ResultOutput.Text;
+            string ExpectedResult = celFarPage.ConversorCelFar(30);
+            Assert.AreEqual(ActualResult, ExpectedResult);
+        }
+
+        [TearDown]
+        //close the page and finish test
+        public void AfterTestCase()
+        {
+            Driver.Quit();
+        }
+
+
+
+    }
+}
